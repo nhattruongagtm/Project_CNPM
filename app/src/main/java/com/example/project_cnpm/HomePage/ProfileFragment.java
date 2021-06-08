73cb3cfa6c435272b80f1f5db97cca258872809f
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2021-06-08 19:54:17 +0700

    hoàn thành đăng xuất tài khoản

--- app/src/main/java/com/example/project_cnpm/HomePage/ProfileFragment.java
+++ app/src/main/java/com/example/project_cnpm/HomePage/ProfileFragment.java
@@ -1,13 +1,17 @@
 package com.example.project_cnpm.HomePage;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,14 +20,22 @@ import com.example.project_cnpm.MainActivity;
 import com.example.project_cnpm.R;
 import com.example.project_cnpm.Model.Customer;
 import com.example.project_cnpm.SharedReferences.DataLocalManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class ProfileFragment extends Fragment {
     private TextView email;
     private ImageView img;
+    private TextView btnSignout;
+
+    GoogleSignInClient mGoogleSignInClient;
 
 
     public ProfileFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -31,12 +43,39 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     public void mapping(View v){
         email = v.findViewById(R.id.emailCustomer);
         img = v.findViewById(R.id.imgCustomer);
+        btnSignout = v.findViewById(R.id.logout);
     }
+    public void createDialogSignOut(){
+        Dialog dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.dialog_signout);
+        Button btnY, btnN;
+        btnN = dialog.findViewById(R.id.no_signout);
+        btnY = dialog.findViewById(R.id.yes_signout);
+
+        btnN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,6 +93,36 @@ public class ProfileFragment extends Fragment {
             Glide.with(getContext()).load(account.getAvatar()).into(img);
         }
 
+        googleAPI();
+
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialogSignOut();
+            }
+        });
+
         return v;
+    }
+    public void signOut(){
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //
+                        //  MainActivity.account = null;
+
+
+                        //getActivity().finish();
+
+                    }
+                });
+        DataLocalManager.setAccount(null);
+        startActivity(new Intent(getActivity(),MainActivity.class));
+    }
+    //login google
+    public void googleAPI(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
     }
 }
